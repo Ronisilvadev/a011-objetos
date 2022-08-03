d50574af468b5e743bf7314247b414599ab47778
roni <ronildo.sad2@hotmail.com>
2022-08-02 21:13:28 -0300

    modificando e adicionando codigos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,13 +1,29 @@
-const arraySacolao = [
-    {nome: "banana"} , 
-    {preco: 3.50} , 
-    {disponibilidade: true}
-]
 
-arraySacolao.push(
-    {nome: "uva" , preco: 5.35 , disponibilidade: false} , 
-    {nome: "morango" , preco: 9.99 , disponibilidade: true} , 
-    {nome: "manga" , preco: 5.35 , disponibilidade: true}
-    )
+//Crie um array vazio chamado `sacolao`, e siga os passos abaixo:
 
-console.log(arraySacolao)
+const arraySacolao = []
+
+
+//Crie três objetos que vão representar frutas do sacolão. Os objetos devem ter as seguintes propriedades
+
+const fruta1 = {
+    nome: "uva" , 
+    preco: 5.35 , 
+    disponibilidade: false
+}
+const fruta2 = {
+    nome: "morango" , 
+    preco: 9.99 , 
+    disponibilidade: true
+} 
+
+const fruta3 = {
+    nome: "manga" , 
+    preco: 5.35 , 
+    disponibilidade: true
+}
+
+//Adicione os objetos ao array `sacolao` utilizando o método **`push()`**
+arraySacolao.push({fruta1 , fruta2 , fruta3})
+
+console.log(sacolao)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -4,29 +4,39 @@ const pokemon1 = {
 	nivel: 5
 }
 
+// Crie uma cópia do objeto, acima, mantendo a estrutura, mas trocando **`nome`** por “Squirtle” e **`tipo`** por “Água”
+
 const copiaPokemon1 = {
     ...pokemon1 , 
     nome: "Squirtle" , 
     tipo: "agua"
 }
 
+//No objeto original, adicione mais uma propriedade, chamada `ataques`. Esta propriedade deve guardar um **array** de objetos com a estrutura abaixo. Inicie a propriedade como um array vazio, e crie o objeto abaixo utilizando `**push()**`
 
-pokemon1.ataques = []
+pokemon1.ataques = [] //array vazio
 
-pokemon1.ataques.push({
+
+
+pokemon1.ataques.push([{
     nome: "Investida",
     dano: 40,
     tipo: "Normal",
     precisao: 100,
-}) 
+}])
 
 
-console.log("original" ,pokemon1)
-console.log("copia" , copiaPokemon1)
+console.log("original-" ,pokemon1)
+console.log("copia-" , copiaPokemon1)
 
-copiaPokemon1.ataques = {...pokemon1.ataques}
+//Na cópia do objeto original, adicione a propriedade **`ataques`** a partir do **espalhamento** da propriedade criada para o objeto original;
+
+copiaPokemon1.ataques = [...pokemon1.ataques]
+
 
 console.log("copia", copiaPokemon1)
+
+//para o objeto original, adicione o ataque **“Folha Navalha”,** com **45 de dano**, **100 de precisão**, e de tipo **“Grama”**;
 
 pokemon1.ataques.push([{
     nome: "Folha Navalha",
@@ -38,7 +48,7 @@ pokemon1.ataques.push([{
 console.log("original", pokemon1)
 
 
-copiaPokemon1.ataques = ([{
+copiaPokemon1.ataques.push([{
     nome: "Jato de Água",
     dano: 40,
     tipo: "Água",
@@ -46,10 +56,12 @@ copiaPokemon1.ataques = ([{
 }])
 
 
+
+
 console.log("Final original" , pokemon1)
 console.log("Final copia" , copiaPokemon1)
 
-console.log(copiaPokemon1.ataques[0].tipo)
+//console.log(copiaPokemon1.ataques[0].tipo)
 
 
 
